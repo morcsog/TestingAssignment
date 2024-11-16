96e933f3e8f31eb2a3ca52bc35139179527fa7ca
morcsog <morcsog99@gmail.com>
2024-11-16 19:10:53 +0100

    Passes the second test
    This commit creates a new test, it just instantiates a new TaskList and Task object, uses the the add function to store that instance in a private property and then the get function returns that instance if it's stored, if not, then it'll just return a null.

--- src/Tasks/TaskList.php
+++ src/Tasks/TaskList.php
@@ -4,15 +4,17 @@ declare(strict_types=1);
 
 namespace App\Tasks;
 
+use App\Tasks\Task;
 class TaskList
 {
+    private Task $task;
     public function add(Task $task): void
     {
-
+        $this->task = $task;
     }
 
     public function get(int $intex): ?Task
     {
-        return null;
+        return isset($this->task) ? $this->task : null;
     }
 }
--- tests/Unit/TaskListTest.php
+++ tests/Unit/TaskListTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\Unit;
 
 use App\Tasks\TaskList;
+use App\Tasks\Task;
 use PHPUnit\Framework\TestCase;
 
 //require_once dirname(__DIR__) . '/vendor/autoload.php';
@@ -15,6 +16,13 @@ class TaskListTest extends TestCase
     {
         $taskList = new TaskList();
         $this->assertNull($taskList->get(0));
+    }
 
+    public function testReturnsTheSame()
+    {
+        $taskList = new TaskList();
+        $task = new Task();
+        $taskList->add($task);
+        $this->assertSame($task, $taskList->get(0));
     }
 }
