65036c70bd1cd082b3b099fac9529dea57738da4
morcsog <morcsog99@gmail.com>
2024-11-16 17:57:13 +0100

    Creates two classes
    Creates Task.php and TaskList.php

---
+++ src/TaskList.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Task;
+
+class TaskList
+{
+    private function add(Task $task): void
+    {
+
+    }
+
+    private function get(int $intex): ?Task
+    {
+        
+    }
+}
